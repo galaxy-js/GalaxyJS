1657f97a2986b80429e12770e091ca82d9ccd1a3
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-03-09 20:59:00 -0500

    refactor: minimal API fixtures

---
+++ src/Renderer.js
@@ -0,0 +1,70 @@
+import nextTick from './next-tick.js'
+
+export default class Renderer {
+  constructor (nodes) {
+    this._renders = []
+
+    this._setupRender(nodes)
+  }
+
+  static get TEMPLATE_REGEX () {
+    return /{{(.*?)}}/
+  }
+
+  static getExpression (template) {
+    // Save inline expressions
+    let parsedExpression = ''
+
+    let match
+
+    while (match = template.match(Renderer.TEMPLATE_REGEX)) {
+      const leftText = RegExp.leftContext
+
+      if (leftText) parsedExpression += JSON.stringify(leftText)
+
+      const expression = match[1].trim()
+
+      if (expression) parsedExpression += `${leftText ? ' + ' : ''}(${expression})`
+
+      template = RegExp.rightContext
+    }
+
+    return parsedExpression + (template ? `+ ${JSON.stringify(template)}` : '')
+  }
+
+  _setupRender (nodes) {
+    for (const node of nodes) {
+      if (node.nodeType === Node.TEXT_NODE && node.data.indexOf('{{') > -1) {
+        const transformer = new Function(`
+          with (this) {
+            return ${Renderer.getExpression(node.data)}
+          }
+        `)
+
+        this._renders.push(state => {
+          const transformed = transformer.call(state)
+
+          if (transformed !== node.data) {
+            node.data = transformed
+          }
+        })
+      }
+
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        // TODO: Check attributes binding
+      }
+
+      if (node.firstChild) {
+        this._setupRender(node.childNodes)
+      }
+    }
+  }
+
+  render (state) {
+    nextTick(() => {
+      for (const _render of this._renders) {
+        _render(state)
+      }
+    })
+  }
+}
