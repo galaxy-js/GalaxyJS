949842d0f0df82d580b88def9fdebf99b8858161
Camilo Rodriguez <ccastellanos@conexia.com>
2018-05-03 13:26:08 -0500

    fix: add missing compiled

--- dist/galaxy.js
+++ dist/galaxy.js
@@ -174,75 +174,6 @@ function getEvent (expression) {
 }
 
 /**
- * Match filters to split within a template interpolation
- *
- * @type {RegExp}
- */
-const FILTER_SPLIT_REGEX = /(?<!\|)\|(?!\|)/;
-
-/**
- * @type {RegExp}
- */
-const FILTER_REGEX = /^(?<name>\w+)(?<args>\()?/;
-
-/**
- *
- * @param {string} expression
- * @param {Array.<Function>} filters
- *
- * @return {string}
- */
-function getFiltered (expression, filters) {
-  filters = filters.map(filter => {
-    filter = filter.trim();
-
-    const match = FILTER_REGEX.exec(filter);
-    const args = [];
-
-    if (match.groups.args) {
-      let depth = 1;
-      let start = match[0].length;
-      let index = start;
-      let inDouble = false;
-      let inSingle = false;
-
-      function pushArg () {
-        args.push(filter.slice(start, index - 1).trim());
-      }
-
-      // Get filter arguments
-      loop: while (depth) {
-        const inExpression = !inDouble && !inSingle;
-
-        switch (filter.charAt(index++)) {
-          case '(': inExpression && ++depth; break
-          case '"': !inSingle && (inDouble = !inDouble); break
-          case "'": !inDouble && (inSingle = !inSingle); break
-          case '': break loop
-          case ')': {
-            if (inExpression && depth-- === 1) {
-              pushArg();
-              break loop
-            }
-          } break
-          case ',': {
-            if (inExpression && depth === 1) {
-              pushArg();
-              start = index;
-            }
-          } break
-        }
-      }
-    }
-
-    // Compose filter applier
-    return `$value => ${match.groups.name}($value, ...[${args.join()}])`
-  });
-
-  return `[${filters.join()}].reduce((result, filter) => filter(result), ${expression})`
-}
-
-/**
  * Match text template interpolation
  *
  * @type {RegExp}
@@ -255,6 +186,18 @@ const TEXT_TEMPLATE = /{{(?<expression>.*?)}}/;
  * @type {RegExp}
  */
 const HTML_TEMPLATE = /{{{(?<expression>.*?)}}}/;
+
+/**
+ * Match filters to split within a template interpolation
+ *
+ * @type {RegExp}
+ */
+const FILTER_SPLIT_REGEX = /(?<!\|)\|(?!\|)/;
+
+/**
+ * @type {RegExp}
+ */
+const FILTER_REGEX = /^(?<name>\w+)(?:\((?<args>.*)\))?/;
 
 /**
  * Get a JavaScript expression
@@ -298,6 +241,24 @@ function getExpression (template, escape = true) {
   if (template) expressions.push(`\`${template}\``);
 
   return expressions.join(' + ')
+}
+
+/**
+ *
+ * @param {string} expression
+ * @param {Array.<Function>} filters
+ *
+ * @return {string}
+ */
+function getFiltered (expression, filters) {
+  filters = filters.map(filter => {
+    const { groups } = FILTER_REGEX.exec(filter.trim());
+
+    // Compose filter applier
+    return `$value => ${groups.name}($value, ${groups.args})`
+  });
+
+  return `[${filters.join()}].reduce((result, filter) => filter(result), ${expression})`
 }
 
 /**
