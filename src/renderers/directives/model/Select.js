f651398211b4ee2d7676f19b10d00c9c4faa9f18
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-07-02 18:40:24 -0500

    refactor(directives): fast path for single/multiple select elements

--- src/renderers/directives/model/Select.js
+++ src/renderers/directives/model/Select.js
@@ -11,34 +11,31 @@ export default class SelectRenderer extends BindRenderer {
   }
 
   onChange ({ target }) {
-    let values
-    const multiple = this.target.multiple
+    const { options, multiple } = target
 
-    if (multiple) {
-      values = this.value
+    if (!multiple) {
+      for (const { value, selected } of options) {
+
+        // In non-multiple select we need to set
+        // the raw value since there's no reference
+        if (selected) return this.setValue(value)
+      }
+    } else {
+      const values = this.value
 
       if (!Array.isArray(values)) {
         throw new GalaxyError(
           'Invalid bound value. ' +
-          '*bind directive on select elements with the multiple attribute must have an array bound value.'
+          '*bind directive on select elements with a `multiple` attribute must have an array bound value.'
         )
       }
-    }
 
-    for (const { value, selected } of target.options) {
-      if (selected) {
-        if (!multiple) {
-
-          // In non-multiple select we need to set
-          // the raw value since there's no reference
-          return this.setValue(value)
-        } else if (!values.includes(value)) {
-          values.push(value)
-        }
-      } else if (multiple) {
+      for (const { value, selected } of options) {
         const index = values.indexOf(value)
 
-        if (index > -1) {
+        if (selected) {
+          if (index === -1) values.push(value)
+        } else if (index > -1) {
           values.splice(index, 1)
         }
       }
